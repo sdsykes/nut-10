a03a64066c3f0d9d5d915954766d6e6b706b914d
Stephen Sykes <sdsykes@gmail.com>
2019-05-28 22:31:23 +0300

    Improve test harness to test any impl.

--- test.rb
+++ test.rb
@@ -1,6 +1,14 @@
-#runner = Proc.new {|file| `ruby interpret.rb #{file}`}
-runner = Proc.new {|file| `ruby compile.rb #{file} | cc -x assembler - lib.s; ./a.out`}
-#runner = Proc.new {|file| `go run compile.go <#{file} | cc -x assembler - lib.s; ./a.out`}
+runner = case ARGV[0]
+when 'interpret'
+  Proc.new {|file| `ruby interpret.rb #{file}`}
+when 'ruby'
+  Proc.new {|file| `ruby compile.rb #{file} | cc -x assembler - lib.s; ./a.out`}
+when 'go'
+  Proc.new {|file| `go run compile.go <#{file} | cc -x assembler - lib.s; ./a.out`}
+end
+
+abort("Specify interpret, ruby or go") if runner.nil?
+
 files = ["test1.doggolang", "test2.doggolang", "test3.doggolang", "test4.doggolang", "samantha.doggolang", "precedence.doggolang"]
 results = [11, 15, 105, 19, 64185, 42]
 
